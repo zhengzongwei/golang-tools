2a2c0d677ee7c75929d9d298bf078e53a1c61f70
zhengzongwei <zhengzongwei@foxmail.com>
2021-11-15 18:11:11 +0800

    Update datetime_test.go

--- test/datetime_test.go
+++ test/datetime_test.go
@@ -3,11 +3,18 @@ package main
 import (
 	"fmt"
 	"golang-tools/common/datetime"
+	"math/rand"
 	"testing"
 )
 
-func TestConversionTime(t *testing.T){
-	f := datetime.ConversionTime(2046)
-	fmt.Printf("%s",f)
-	
+func TestConversionTime(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(rand.Int63n(10000))
+		f := datetime.ConversionTime(rand.Int63n(10000))
+		fmt.Printf("%s\n", f)
+	}
+
+	// f := datetime.ConversionTime(2046)
+	// fmt.Printf("%s",f)
+
 }
