baa1b92f76fffcd29d58b38d4993e174c552a677
zhengzongwei <zhengzongwei@foxmail.com>
2021-11-24 15:37:04 +0800

    添加获取磁盘信息

--- common/system/system.go
+++ common/system/system.go
@@ -9,20 +9,56 @@ package system
 
 import (
 	"fmt"
-
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/disk"
 	"github.com/shirou/gopsutil/host"
 )
 
+type DiskInfo struct {
+	diskinfo disk.PartitionStat
+	usage disk.UsageStat
+}
+
 type NodeInfo struct{
 	os *host.InfoStat
+	cpu[] cpu.InfoStat
+	disk DiskInfo
 }
+
+/*
+ * @title: SystemInfo
+ * @description: 获取系统相关信息
+ * @param:
+ * @return:
+ */
 
 func SystemInfo(){
 	nodeInfo,_:= host.Info()
+	cpuInfo, _:= cpu.Info()
+
+	diskInfo,_:=disk.Partitions(true)
+	//for i:=0;i<len(diskInfo);i++{
+	//	fmt.Printf("%v\n",diskInfo[i])
+	//}
+	var diskinfo DiskInfo = DiskInfo{ }
+	for _,value :=range diskInfo{
+
+
+		diskusage,_ := disk.Usage(value.Device)
+		diskinfo.diskinfo = value
+		diskinfo.usage = *diskusage
+	}
+
+
 	info :=NodeInfo{
 		os:nodeInfo,
+		cpu:cpuInfo,
+		disk:diskinfo,
 	}
 	
-	fmt.Printf("%v",info.os)
+	fmt.Printf("%v\n",diskinfo)
+	//fmt.Printf("%v\n",diskinfo.usage)
+	fmt.Printf("%v\n",info.cpu)
+	fmt.Printf("%v\n",info.disk)
  }
 
